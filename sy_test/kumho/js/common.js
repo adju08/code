6dced573c3ae1ab46b6170dd825330a15b4cd04d
adju08 <haunje98@naver.com>
2025-06-05 20:58:04 +0900

    .

--- sy_test/kumho/js/common.js
+++ sy_test/kumho/js/common.js
@@ -20,9 +20,63 @@ let scrolling
 let scroll_prev
 let window_w
 let mobile_size = 1024
+let menu_open
 
 
 $(window).scroll(function(){
+    scroll_chk()
+})
+$(window).resize(function(){
+    resize_chk()
+})
+$(document).ready(function(){
+    scroll_chk()
+    resize_chk()
+
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        if(device_status == 'pc'){
+            $('header').addClass('menu_over')
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+            $(this).addClass('over')
+        }
+    })
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
+        $('header').removeClass('menu_over')
+        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+    })
+
+
+    /************* 모바일 메뉴 열고 닫기 ************/
+    $('header .gnb .gnb_open').on('click', function(){
+        $('header').addClass('menu_open')
+    })
+    $('header .gnb .gnb_close').on('click', function(){
+        $('header').removeClass('menu_open')
+    })
+
+    /************* 모바일 2차 메뉴 열고 닫기 *************
+     * 지금 현재 메뉴가 열려있는지 닫혀있는지 구분 (li에 open 클래스 있는지 유무)
+     * 메뉴가 열려있으면 - li에 open클래스 삭제, 2차 메뉴 접기
+     * 메뉴가 닫혀있으면 - li에 open클래스 추가, 2차 메뉴 열기
+     */
+    $('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
+        if(device_status == 'mobile'){
+            e.preventDefault()
+            menu_open = $(this).parents('li').hasClass('open')
+            // console.log(menu_open)
+
+            if(menu_open == true){
+                $(this).parents('li').removeClass('open')
+                $(this).next().slideUp()
+            }else{
+                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('open')
+                $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2').slideUp()
+                $(this).parents('li').addClass('open')
+                $(this).next().slideDown()
+            }
+        }
+    })
+
 
 })
 
@@ -36,10 +90,16 @@ function resize_chk(){
     }
 }
 function scroll_chk(){
+    scroll_prev = scrolling
     scrolling = $(window).scrollTop()
     if(scrolling > 0){
         $('header').addClass('fixed')
+        if(scrolling > scroll_prev){
+            $('header').addClass('gnb_up')
+        }else{
+            $('header').removeClass('gnb_up')
+        }
     }else{
-
+        $('header').removeClass('fixed')
     }
 }
