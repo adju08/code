0abd55908afe126a00b7b80c4e526b83d39863cd
adju08 <haunje98@naver.com>
2025-06-26 18:03:17 +0900

    .

--- leeeungno/js/main.js
+++ leeeungno/js/main.js
@@ -55,6 +55,43 @@ $(document).ready(function(){
 
 
 
+/********* exhibitions 연결 : 시작 *********/
+const exhibitions_swiper = new Swiper('.exhibitions .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+	breakpoints: {
+		768: {    /* 640px 이상일때 적용 */
+			spaceBetween: 24,
+		},
+	},
+	//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	
+});
+/********* exhibitions 연결 : 종료 *********/
+
+
+
+
+/********* public 연결 : 시작 *********/
+const public_swiper = new Swiper('.public .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 0, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 768px 이상일때 적용 */
+                spaceBetween: 0,
+                centeredSlides: true,
+            },
+        },
+        centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.public .list .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            //type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+    });
+/********* public 연결 : 종료 *********/
 
 
 
