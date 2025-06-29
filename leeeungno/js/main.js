be0f83e644352fd0ef68cd5897b0961ceafa7f7f
adju08 <haunje98@naver.com>
2025-06-29 17:50:29 +0900

    250629 이응노 미술관 완성

--- leeeungno/js/main.js
+++ leeeungno/js/main.js
@@ -114,8 +114,12 @@ const collection_swiper = new Swiper('.collection .list_mobil .swiper', { /* 팝
 	// 	delay: 2500,
 	// 	disableOnInteraction: true,
 	// },
+    navigation: {
+		nextEl: '.collection .ctrl_wrap .btn_next',
+		prevEl: '.collection .ctrl_wrap .btn_prev',
+	},
 	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		el: '.collection .ctrl_wrap .paging', /* 해당 요소의 class명 */
 		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
 		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
 	},
