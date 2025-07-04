f4ab1c4fb0c1c0e03b6c741d209a5b387aaf3ef7
adju08 <haunje98@naver.com>
2025-07-04 18:04:03 +0900

    .

--- hyundaicorp/js/main.js
+++ hyundaicorp/js/main.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
 
-    AOS.init({
-        offset: 200, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
-        duration: 500, // 애니메이션 효과가 작동되는 시간
-        easing: 'ease', // 가속도
-    });
-
     
     const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
