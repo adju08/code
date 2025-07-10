68718e25f629a0e67613958004930e823f4f265f
adju08 <haunje98@naver.com>
2025-07-10 18:02:27 +0900

    .

---
+++ sku/js/main.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function(){
+    //console.log('들어가니')
+
+    AOS.init({
+        offset: 200, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+        duration: 500, // 애니메이션 효과가 작동되는 시간
+        easing: 'ease', // 가속도
+    });
+
+
+/*************************** visual swiper 연결 : 시작 ***************************/
+    let visual_name = ['Seokyeong Univ', 'Ideas, together', '2nd in jobs, career aid']
+    //console.log(visual_name[3]) 숫자가 0부터 시작
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 5000,
+        //     disableOnInteraction: true,
+        // },
+
+        //effect: "fade", /* fade 효과 */
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.visual .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            //type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+                return '<span class="' + className + '">  ' + visual_name[index] + "</span>";
+            },
+        },
+    });
+
+/*************************** visual swiper 연결 : 종료 ***************************/
+
+
+
+
+
+
+}) //$(document).ready
